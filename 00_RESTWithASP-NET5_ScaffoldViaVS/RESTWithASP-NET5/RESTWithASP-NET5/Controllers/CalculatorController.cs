10b012378b85c7ba78eb996a14669055c720430b
unknown <costaalves.danilo@gmail.com>
2021-04-06 17:32:04 -0300

    Implementing calculator

---
+++ 00_RESTWithASP-NET5_ScaffoldViaVS/RESTWithASP-NET5/RESTWithASP-NET5/Controllers/CalculatorController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTWithASP_NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("multi/{firstNumber}/{secondNumber}")]
+        public IActionResult Multi(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (ValidDivOperation(firstNumber, secondNumber))
+                {
+                    var sub = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+
+                    return Ok(sub.ToString());
+                }
+                else
+                    return BadRequest("Invalid Operation. First number can't be zero.");
+
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+
+                return Ok(media.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("sqrt/{firstNumber}")]
+        public IActionResult Sqrt(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var sqrt = Math.Sqrt(ConvertToDouble(firstNumber));
+
+                return Ok(sqrt.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber,
+                                            System.Globalization.NumberStyles.Any,
+                                            System.Globalization.NumberFormatInfo.InvariantInfo,
+                                            out number);
+            return isNumber;
+        }
+
+        private double ConvertToDouble(string strNumber)
+        {
+            double doubleValue;
+
+            if (double.TryParse(strNumber, out doubleValue))
+                return doubleValue;
+
+            return 0;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+                return decimalValue;
+
+            return 0;
+        }
+
+        private bool ValidDivOperation(string firstNumber, string secondNumber)
+        {
+            if (ConvertToDecimal(firstNumber) == 0 && ConvertToDecimal(secondNumber) > 0)
+                return false;
+
+            return true;
+        }
+    }
+}
